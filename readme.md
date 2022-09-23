3abafd7f4d3e9a7e1aa3e2a97ec9b6fb5517389d
Ignacio Lopez <Nacho.ios@icloud.com>
2022-09-23 15:27:28 -0300

    Update

--- readme.md
+++ readme.md
@@ -3,6 +3,7 @@
 A partir de un archivo JSON (Flags.json), podremos obtener las URL de imágenes
 necesarias para posteriormente colocarlas en el Array $specificUrl, previamente
 separar dichos strings obtenidos por un espacio, para poder llamar individualmente
-a los URL.
+a los URL y descargarlos.
 
 Número identificador de las banderas: del 0 al 95.
+Banderas totales: 96
--- scrapingFlags.php
+++ scrapingFlags.php
@@ -32,54 +32,6 @@ foreach($specificUrl as $imagen)
 echo "Downloaded!";
 
 
-
-
-
-// foreach ($decodedJson->data as $i => $flagFound){
-    
-    
-
-// }
-
-
-
-
-
-// $img = "foto";
-
-// file_put_contents($img, file_get_contents($flagFound[0]));
-  
-// echo "File downloaded!"
- 
-
-// echo $i. ': '. $flagFound->flag_url;  
-// echo $i. ': '. $flagFound->flag_url . " "; //Posee un espacio para mejor lectura en pantalla
-// echo $flagFound->flag_url . " ";  //Imprimimos en pantalla solo las URL
-
-/*if($flagFound){
-    echo "ok";
-}   else {
-    echo "error";
-}*/
-
-
-// foreach ($decodedJson->data as $i => $flagFound){ 
-//     echo $i. ': '. $flagFound->flag_url . " "; //Posee un espacio para mejor lectura en pantalla
-   
-// }
-
-
-// foreach ($decodedJson->data as $i => $flagFound){
-//     if ($i == "Afghanistan.png"){
-//         echo $flagFound->flag_url . " "; // Print de un pais en específico
-//     }
-
-// }
-
-
-
-
-    
  
 ?>
 
